2c55900e25a3c864a10a64af03e37efbefd96774
Tom Milligan <tom@tommilligan.net>
2017-03-06 23:51:58 +0000

    Completed about page

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="hello">
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><router-link to="/">Enter</router-link></li>
-      <br />
-      <li><router-link to="/linechart">Test d3 and sigma are working</router-link></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>synaptic-scout | isoprene-pumpjack</h2>
+    <div class="credits">
+      <div v-for="section in sections" :key="section.title" :class="section.className">
+        <h4>{{section.title}}</h4>
+        <ul>
+          <li v-for="credit in section.credits">
+            <a :href="credit.url" target="_blank">
+              <img :src="credit.logo" class="logos" :alt="credit.alt">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,26 +21,115 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      sections: [
+        {
+          title: 'displayed with',
+          className: 'credits-js',
+          credits: [
+            {
+              alt: 'VueJS',
+              url: 'https://vuejs.org/',
+              logo: require('../assets/logos/vue.svg')
+            },
+            {
+              alt: 'D3',
+              url: 'https://d3js.org/',
+              logo: require('../assets/logos/d3.svg')
+            },
+            {
+              alt: 'Webpack',
+              url: 'https://webpack.js.org/',
+              logo: require('../assets/logos/webpack.svg')
+            }
+          ]
+        },
+        {
+          title: 'crunched with',
+          className: 'credits-python',
+          credits: [
+            {
+              alt: 'Python',
+              url: 'https://www.python.org/',
+              logo: require('../assets/logos/python.svg')
+            },
+            {
+              alt: 'Flask',
+              url: 'http://flask.pocoo.org/',
+              logo: require('../assets/logos/flask.svg')
+            },
+            {
+              alt: 'Redis',
+              url: 'https://redis.io/',
+              logo: require('../assets/logos/redis.svg')
+            }
+          ]
+        },
+        {
+          title: 'stored with',
+          className: 'credits-built',
+          credits: [
+            {
+              alt: 'Neo4J',
+              url: 'https://neo4j.com/',
+              logo: require('../assets/logos/neo4j.svg')
+            },
+            {
+              alt: 'Elasticsearch',
+              url: 'https://www.elastic.co/',
+              logo: require('../assets/logos/elasticsearch.svg')
+            }
+          ]
+        },
+        {
+          title: 'built with',
+          className: 'credits-built',
+          credits: [
+            {
+              alt: 'GitHub',
+              url: 'https://github.com/',
+              logo: require('../assets/logos/github-icon-1.svg')
+            },
+            {
+              alt: 'TravisCI',
+              url: 'https://travis-ci.org/',
+              logo: require('../assets/logos/travis-ci.svg')
+            },
+            {
+              url: 'https://www.heroku.com/',
+              logo: require('../assets/logos/heroku.svg')
+            }
+          ]
+        }
+      ]
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
+<style scoped lang="scss">
+h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+.credits {
+  $logo-size: 64px;
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    height: $logo-size;
+    width: $logo-size;
+  }
+
+    .logos {
+    height: $logo-size;
+  }
 }
 
 a {
